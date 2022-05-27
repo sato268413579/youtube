7fd49aec1bda439ff22f623cd85e8963d286eda0
sato268413579 <aderu1995@gmail.com>
2022-05-27 14:06:40 +0900

    youtubeもどき実装中

--- urls.py
+++ urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('submit_post', views.submit_post, name='submit_post'),
 ]
--- views.py
+++ views.py
@@ -1,4 +1,31 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h1>Hello, myapp1!</h1>')
+    import os
+    import MySQLdb
+    import mysql.connector
+
+    # コネクションの作成
+    conn = mysql.connector.connect(
+        host='localhost',
+        port='3306',
+        user='root',
+        password='password',
+        database='youtube_python'
+    )
+
+    # DB操作用にカーソルを作成
+    cur = conn.cursor(MySQLdb.cursors.DictCursor)
+
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+
+    # DB操作が終わったらカーソルとコネクションを閉じる
+    cur.close()
+    conn.close()
+    
+    return render(request, os.getcwd() + '\youtube\\templates\youtube.html', {'title':'Hello!!!', 'rows': rows})
+
+def submit_post(request):
+    return HttpResponse('<h1>OK!<h1/>')
